639c2d0d5acc578c5383510c39b3618d6a96bea5
PUNIT-SONI-2006 <84916673+PUNIT-SONI-2006@users.noreply.github.com>
2023-12-02 15:45:59 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import cv2
+img=cv2.imread("solar-system.jpg")
+cv2.putText(img,"Sun",(100,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Mercury",(120,250),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Venus",(190,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Earth",(290,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Mars",(390,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Jupiter",(550,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Saturn",(780,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Uranus",(980,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Neptune",(1100,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+
+cv2.imshow("output",img)
+
+cv2.waitKey(0)
